67878d0af3640b5495240c30b050ffa2080c63ed
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-09-01 01:45:17 +0300

    Modifed source and target pair samples

--- scripts/m4t/evaluate/download_data.py
+++ scripts/m4t/evaluate/download_data.py
@@ -6,11 +6,9 @@
 
 import logging
 import os
-
 import scipy.io.wavfile as wavfile
 import torch
-from seamless_communication.datasets.huggingface import \
-    Speech2SpeechFleursDatasetBuilder
+from seamless_communication.datasets.huggingface import Speech2SpeechFleursDatasetBuilder
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,42 +17,39 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-
 def make_directories(*paths):
     for path in paths:
         os.makedirs(path, exist_ok=True)
-
 
 def combine_texts(texts, output_path):
     with open(output_path, "w") as output_file:
         for text in texts:
             output_file.write(text + "\n")
 
-
-def download_datasets(languages, num_datasets_per_language, output_directory):
+def download_datasets(language_pairs, num_datasets, output_directory):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    for lang in languages:
-        lang_output_dir = os.path.join(output_directory, lang)
-        lang_source_audio_dir = os.path.join(lang_output_dir, "source_audio")
-        lang_target_text_dir = os.path.join(lang_output_dir, "target_text")
-        lang_source_text_dir = os.path.join(lang_output_dir, "source_text")
-        lang_target_audio_dir = os.path.join(lang_output_dir, "target_audio")
+    for source_lang, target_lang in language_pairs:
+        lang_pair = f"{source_lang}_{target_lang}"
+
+        lang_output_dir = os.path.join(output_directory, lang_pair)
+        lang_source_audio_dir = os.path.join(lang_output_dir, f"source_audio_{source_lang}")
+        lang_target_texts_dir = os.path.join(lang_output_dir, f"target_texts_{target_lang}")
 
         make_directories(
             lang_output_dir,
             lang_source_audio_dir,
-            lang_target_text_dir,
-            lang_source_text_dir,
-            lang_target_audio_dir,
+            lang_target_texts_dir,
         )
 
-        logger.info(f"Downloading datasets for language: {lang}")
+        logger.info(f"Downloading datasets for language pair: {lang_pair}")
+
+        # cache_dir = output_directory / "cache"
 
         dataset_builder = Speech2SpeechFleursDatasetBuilder(
-            source_lang=lang,
-            target_lang=lang,
+            source_lang=source_lang,
+            target_lang=target_lang,
             split="test",
             skip_source_audio=False,
             skip_target_audio=False,
@@ -63,32 +58,24 @@ def download_datasets(languages, num_datasets_per_language, output_directory):
             speech_tokenizer=None,
         )
 
-        dataset_count = 0
-        source_texts = []
+        dataset_count = 1
         target_texts = []
 
-        for lang_pair_sample in dataset_builder:
+        for _, lang_pair_sample in enumerate(dataset_builder):
+          
             source_sample = lang_pair_sample.source
             target_sample = lang_pair_sample.target
 
             source_audio = source_sample.waveform.numpy()
-            target_audio = target_sample.waveform.numpy()
-
-            source_text = source_sample.text
             target_text = target_sample.text
 
             source_audio_path = os.path.join(
                 lang_source_audio_dir, f"source_{dataset_count}.wav"
             )
-            target_audio_path = os.path.join(
-                lang_target_audio_dir, f"target_{dataset_count}.wav"
-            )
 
             # Save audio data as WAV files
             wavfile.write(source_audio_path, source_sample.sampling_rate, source_audio)
-            wavfile.write(target_audio_path, target_sample.sampling_rate, target_audio)
 
-            source_texts.append(source_text)
             target_texts.append(target_text)
 
             logger.info(
@@ -104,24 +91,21 @@ def download_datasets(languages, num_datasets_per_language, output_directory):
             print("=" * 100)
 
             dataset_count += 1
-            if dataset_count >= num_datasets_per_language:
+            if dataset_count >= num_datasets:
                 break
 
         combine_texts(
-            source_texts, os.path.join(lang_source_text_dir, "source_references.txt")
-        )
-        combine_texts(
-            target_texts, os.path.join(lang_target_text_dir, "target_references.txt")
+            target_texts, os.path.join(lang_target_texts_dir, "target_texts_references.txt")
         )
 
         logger.info(
-            f"Downloaded and saved {dataset_count} datasets for language: {lang}"
+            f"Downloaded and saved {dataset_count} datasets for language pair: {lang_pair}"
         )
         print("=" * 100)
 
-
-languages = ["hi_in", "af_za"]
-num_datasets_per_language = 10
+# List of language tuples for translation
+language_pairs = [("en_us", "fr_fr")]  # Add more language pairs if needed
+num_datasets_per_language = 5
 output_directory = "./downloaded_data"
 
-download_datasets(languages, num_datasets_per_language, output_directory)
+download_datasets(language_pairs, num_datasets_per_language, output_directory)
