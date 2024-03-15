fixing mixed precision training

src/seamless_communication/cli/expressivity/train/prosody_unity2_train.py
@@ -7,51 +7,18 @@
 import os
 from datetime import timedelta
 from pathlib import Path
-
 import torch
+
+from fairseq2 import setup_extensions
 from seamless_communication.train.unity2.trainer import UnitYTrainConfig, load_unity_trainer
 from seamless_communication.models.unity import unity_archs
-# from seamless_next.cluster import Cluster, ClusterConfig
+from seamless_next.cluster import Cluster, ClusterConfig
 
 
-# def main() -> None:
-
-#     user = os.getenv("USER")
-
-#     output_dir = Path(f"/checkpoint/{user}/nllb100-example")
-
-#     cluster_config = ClusterConfig(
-#         cluster="local",
-#         parallelism=1,
-#         partition="",
-#         num_nodes=1,
-#         num_gpus_per_node=1,
-#         cpus_per_task=1,
-#         log_dir=output_dir.joinpath("submitit"),
-#         timeout=timedelta(minutes=1000),
-#     )
-
-#     cluster = Cluster(cluster_config)
-
-#     # Load Jean's new 128k NLLB tokenizer.
-#     tokenizer = load_nllb_tokenizer("nllb-100-128k")
-
-#     model_config = nllb_archs.get_config("dense_1b")
-
-#     # We adapt the existing NLLB-200 dense 1B to 100 languages.
-#     model_config.vocab_info = tokenizer.vocab_info
-
-#     train_config = NllbTrainConfig(
-#         model_config_or_name=model_config,
-#         tokenizer_name="nllb-100-128k",
-#         dataset_name="nllb-100-dataset",
-#         output_dir=output_dir,
-#         parallelism="fsdp",
-#         dtype=torch.float16,
-#         debug=True,
-#     )
-
-#     cluster.run_job(train_nllb, train_config)
+def train_unity(config: UnitYTrainConfig) -> None:
+    setup_extensions()
+    trainer = load_unity_trainer(config)
+    trainer.run()
 
 
 if __name__ == "__main__":
@@ -63,13 +30,13 @@ if __name__ == "__main__":
         dataset_name="seamless_expressive_eng_cmn",
         gcmvn_prosody_input=True,
         gcmvn_stats_name="vocoder_pretssel",
-        output_dir=Path("~/tmp/test"),
-        dtype="torch.float16",
-        parallelism="none",
-        num_prefetch=3,
+        output_dir=Path("training_logs/"),
+        dtype="torch.bfloat16",
+        parallelism="fsdp",
+        num_prefetch=4,
         shuffle_window_size=0,
-        use_submitit=False,
-        gradient_accumulation=1,
+        publish_metrics_every_n_steps=1,
+        gradient_accumulation=4,
         ignore_text_prefix_size=1,
         aux_loss_type="ctc",
         aux_loss_weight=1.6,
@@ -78,6 +45,18 @@ if __name__ == "__main__":
         load_prosody_encoder_weight="/fsx-ust/shared/seamless23_oss/prosody_encoder_pretssel_pretrained.pt",
     )
 
-    trainer = load_unity_trainer(train_config)
+    cluster_config = ClusterConfig(
+        cluster="local",
+        parallelism=1,
+        partition="",
+        num_nodes=1,
+        num_gpus_per_node=8,
+        cpus_per_task=10,
+        log_dir=train_config.output_dir.joinpath("submitit"),
+        timeout=timedelta(minutes=1000),
+    )
 
-    trainer.run()
+    cluster = Cluster(cluster_config)
+
+    cluster.run_job(train_unity, train_config)
+    # train_unity(train_config)

src/seamless_communication/models/generator/ecapa_tdnn.py
@@ -134,12 +134,12 @@ class ECAPA_TDNN(Module):
 
         # Attentive Statistical Pooling
         x = self.asp(x, padding_mask=padding_mask)
-        x = self.asp_norm(x.transpose(1, 2)).transpose(1, 2)
+        x = self.asp_norm(x.squeeze(-1)).unsqueeze(-1)
 
         # Final linear transformation
         x = self.fc(x)
 
-        x = x.transpose(1, 2).squeeze(1)  # B x C
+        x = x.squeeze(-1)  # B x C
         return F.normalize(x, dim=-1)
 
 

src/seamless_communication/models/unity/model.py
@@ -154,6 +154,7 @@ class UnitYModelOutput:
             ignore_prefix_size=ignore_unit_prefix_size,
             label_smoothing=label_smoothing,
         )
+        unit_loss = loss.detach().data
 
         if text_loss_weight > 0.:
             text_nll_loss = self.text_output.compute_loss(
@@ -166,7 +167,7 @@ class UnitYModelOutput:
         # calculate duration loss
         if duration_loss_weight > 0.:
             log_durations = apply_padding_mask(self.log_durations, self.char_padding_mask)
-            duration_target = torch.log(self.attn_hard_dur + 1)
+            duration_target = torch.log(self.attn_hard_dur + 1).to(log_durations)
             duration_loss = mse_loss(log_durations, duration_target, reduction="sum")
             loss += duration_loss_weight * duration_loss
 
@@ -202,7 +203,7 @@ class UnitYModelOutput:
             if aux_loss_type == "ctc":
                 with torch.backends.cudnn.flags(enabled=False):
                     aux_loss = ctc_loss(
-                        aux_lprobs.transpose(0, 1),
+                        aux_lprobs.transpose(0, 1).float(),
                         self.char_seqs,
                         unit_lens,
                         char_lens,
@@ -214,7 +215,13 @@ class UnitYModelOutput:
 
             loss += aux_loss_weight * aux_loss
 
-        return loss
+        return loss, {
+            "unit_nll_loss": unit_loss,
+            "text_nll_loss": text_nll_loss.detach().data,
+            "duration_loss": duration_loss.detach().data,
+            "forward_sum_loss": forward_sum_loss.detach().data,
+            "aux_loss": aux_loss.detach().data,
+        }
 
 
 @final
@@ -695,7 +702,6 @@ class UnitYNART2UModel(Module):
         # text_seqs: (N, S)
         seqs, padding_mask, log_durations, char_seqs, char_padding_mask, attn_lprob, attn_hard_dur = self.decoder_frontend(
             encoder_output,
-            encoder_padding_mask,
             text_seqs,
             unit_seqs,
             duration_factor,

src/seamless_communication/train/unity2/dataset.py
@@ -31,7 +31,11 @@ from fairseq2.data.text import (
     TextTokenizer,
     read_text,
 )
-from fairseq2.data.audio import AudioDecoder, WaveformToFbankConverter, WaveformToFbankOutput
+from fairseq2.data.audio import (
+    AudioDecoder,
+    WaveformToFbankConverter,
+    WaveformToFbankOutput,
+)
 from fairseq2.datasets.error import DatasetError
 from fairseq2.datasets.loader import StandardDatasetLoader
 from fairseq2.datasets.utils import all_eod
@@ -80,6 +84,7 @@ class UnitYDataset:
         text_tokenizer: TextTokenizer,
         unit_tokenizer: UnitTokenizer,
         gang: Gang,
+        dtype: DataType,
         max_seq_len: int,
         max_num_tokens: int,
         *,
@@ -108,45 +113,80 @@ class UnitYDataset:
             gcmvn_std=gcmvn_std,
         )
 
-        while True:
-            eod = False
+        eod = False
 
-            pipeline_iter = iter(pipeline)
+        pipeline_iter = iter(pipeline)
 
-            while not eod:
-                batches = []
+        stored_batches = []
 
-                for _ in range(num_accumulate):
-                    try:
-                        example = next(pipeline_iter)
-                    except StopIteration:
-                        break
+        while not eod:
+            batches = []
 
-                    batch = self._example_to_batch(example, gang.device)
+            for _ in range(num_accumulate):
+                try:
+                    example = next(pipeline_iter)
+                except StopIteration:
+                    break
 
-                    batches.append(batch)
+                batch = self._example_to_batch(example, gang.device, dtype)
 
-                eod = len(batches) != num_accumulate
+                batches.append(batch)
 
-                # When the pipeline is sharded, sampling and bucketing by length
-                # can lead to unpredictability in the number of examples read in
-                # each process. So, it is important to ensure that all processes
-                # are in sync about the end of the data. If this is not the case,
-                # a training loop may become stuck.
-                if bucket_by_length:
-                    eod = all_eod(eod, gang, logger)
+            eod = len(batches) != num_accumulate
 
-                if not eod:
-                    yield batches
+            # When the pipeline is sharded, sampling and bucketing by length
+            # can lead to unpredictability in the number of examples read in
+            # each process. So, it is important to ensure that all processes
+            # are in sync about the end of the data. If this is not the case,
+            # a training loop may become stuck.
+            if bucket_by_length:
+                eod = all_eod(eod, gang, logger)
+
+            if not eod:
+                yield batches
+                stored_batches.append(self.move_batch(batches, torch.device("cpu")))
+
+        else:
+            num_epoch = 1
+            while True:
+                num_epoch += 1
+                logger.info(f"Starting on the {num_epoch}th epoch")
+                for batches in stored_batches:
+                    yield self.move_batch(batches, gang.device)
 
     @staticmethod
-    def _example_to_batch(example: Dict[str, Any], device: Device) -> UnitYBatch:
+    def move_batch(batches: List[UnitYBatch], device: Device) -> List[UnitYBatch]:
+        for batch in batches:
+            batch.source_seqs = batch.source_seqs.to(device)
+            if batch.source_padding_mask is not None:
+                batch.source_padding_mask = batch.source_padding_mask.to(device)
+
+            batch.target_seqs = batch.target_seqs.to(device)
+            if batch.target_padding_mask is not None:
+                batch.target_padding_mask = batch.target_padding_mask.to(device)
+
+            batch.prosody_input_seqs = batch.prosody_input_seqs.to(device)
+            if batch.prosody_input_padding_mask is not None:
+                batch.prosody_input_padding_mask = batch.prosody_input_padding_mask.to(device)
+
+            batch.target_text_seqs = batch.target_text_seqs.to(device)
+            if batch.target_text_padding_mask is not None:
+                batch.target_text_padding_mask = batch.target_text_padding_mask.to(device)
+
+        return batches
+
+    @staticmethod
+    def _example_to_batch(
+        example: Dict[str, Any], device: Device, dtype: DataType
+    ) -> UnitYBatch:
         sample = example["sample"]
         source_data = cast(SequenceData, sample["audio"]["data"]["fbank"])
         target_text_data = cast(SequenceData, sample["raw_tgt_text"])
         target_data = cast(SequenceData, sample["tgt_text"])
         prosody_input_seqs = None
-        if (gcmvn_fbank := sample["audio"]["data"].get("gcmvn_fbank", None)) is not None:
+        if (
+            gcmvn_fbank := sample["audio"]["data"].get("gcmvn_fbank", None)
+        ) is not None:
             prosody_input_data = cast(SequenceData, gcmvn_fbank)
             prosody_input_seqs, prosody_input_padding_mask = get_seqs_and_padding_mask(
                 prosody_input_data, device
@@ -163,11 +203,11 @@ class UnitYDataset:
         )
 
         return UnitYBatch(
-            source_seqs=source_seqs,
+            source_seqs=source_seqs.to(dtype),
             source_padding_mask=source_padding_mask,
             target_seqs=target_seqs,
             target_padding_mask=target_padding_mask,
-            prosody_input_seqs=prosody_input_seqs,
+            prosody_input_seqs=prosody_input_seqs.to(dtype),
             prosody_input_padding_mask=prosody_input_padding_mask,
             target_text_seqs=target_text_seqs,
             target_text_padding_mask=target_text_padding_mask,
@@ -181,7 +221,7 @@ class UnitYDataset:
 
     @staticmethod
     def squeeze_Tensor(tensor: Tensor) -> Tensor:
-        #TODO: loose unit_tokenizer input requirement to support 1D
+        # TODO: unit_tokenizer to support 1D tensor input
         return tensor[0]
 
     @staticmethod
@@ -287,8 +327,8 @@ class UnitYDataset:
                         self.normalize_fbank,
                         gcmvn_prosody_input=gcmvn_prosody_input,
                         gcmvn_mean=gcmvn_mean,
-                        gcmvn_std=gcmvn_std
-                    )
+                        gcmvn_std=gcmvn_std,
+                    ),
                 ],
                 selector="audio.data",
                 num_parallel_calls=num_prefetch,
@@ -302,7 +342,10 @@ class UnitYDataset:
             pipeline_builder.map(
                 [
                     text_token_encoder,
-                    partial(self.prepend_eos_symbol, eos_idx=text_tokenizer.vocab_info.eos_idx)
+                    partial(
+                        self.prepend_eos_symbol,
+                        eos_idx=text_tokenizer.vocab_info.eos_idx,
+                    ),
                 ],
                 selector="raw_tgt_text",
                 num_parallel_calls=num_prefetch,
@@ -316,7 +359,11 @@ class UnitYDataset:
                 unit_token_encoder.is_nar_decoder = False
 
             pipeline_builder.map(
-                [self.preprocess_unit_to_Tensor, unit_token_encoder, self.squeeze_Tensor],
+                [
+                    self.preprocess_unit_to_Tensor,
+                    unit_token_encoder,
+                    self.squeeze_Tensor,
+                ],
                 selector="tgt_text",
                 num_parallel_calls=num_prefetch,
             )
@@ -327,9 +374,7 @@ class UnitYDataset:
             # for troubleshooting.
             split_ = DataPipeline.constant(dataset_name).and_return()
 
-            line_nr = DataPipeline.count(
-                start=gang.rank, step=gang.size
-            ).and_return()
+            line_nr = DataPipeline.count(start=gang.rank, step=gang.size).and_return()
 
             # Zip the pipelines along with the pseudo pipelines into one
             pipeline_bld = DataPipeline.zip(
@@ -339,7 +384,7 @@ class UnitYDataset:
 
             pipeline_blds.append(pipeline_bld.and_return())
 
-        concat_pipeline_blds = DataPipeline.concat(pipeline_blds)
+        concat_pipeline_blds = DataPipeline.round_robin(pipeline_blds)
 
         # Shuffle examples.
         if shuffle_window_size > 0:
@@ -354,7 +399,7 @@ class UnitYDataset:
 
             concat_pipeline_blds.bucket_by_length(
                 bucket_sizes,
-                selector="sample.audio.data.fbank",
+                selector="sample.audio.data.fbank,sample.tgt_text,sample.raw_tgt_text",
                 skip_long_examples=True,
             )
         else:

src/seamless_communication/train/unity2/metrics.py
@@ -20,8 +20,11 @@ from seamless_communication.models.unity.model import UnitYBatch
 class UnitYMetricBag(MetricBag):
     """Holds the common metrics of a UnitY model."""
 
-    loss: Mean
-    entropy_loss: Mean
+    unit_nll_loss: Mean
+    text_nll_loss: Mean
+    duration_loss: Mean
+    forward_sum_loss: Mean
+    aux_loss: Mean
     batch_size: Mean
     elements_per_batch: Mean
     elements_per_second: Throughput
@@ -38,9 +41,11 @@ class UnitYMetricBag(MetricBag):
 
         d = gang.device
 
-        self.register_metric("loss", Mean(device=d), persistent=False)
-
-        self.register_metric("entropy_loss", Mean(device=d), persistent=False)
+        self.register_metric("unit_nll_loss", Mean(device=d), persistent=False)
+        self.register_metric("text_nll_loss", Mean(device=d), persistent=False)
+        self.register_metric("duration_loss", Mean(device=d), persistent=False)
+        self.register_metric("forward_sum_loss", Mean(device=d), persistent=False)
+        self.register_metric("aux_loss", Mean(device=d), persistent=False)
 
         self.register_metric("batch_size", Mean(device=d), persistent=False)
 
@@ -68,27 +73,42 @@ class UnitYMetricBag(MetricBag):
         :param elapsed_time:
             The total elapsed time to read and process ``batches``.
         """
-        loss = torch.zeros((), dtype=torch.float64)
+        unit_nll_loss = torch.zeros((), dtype=torch.float64)
+        text_nll_loss = torch.zeros((), dtype=torch.float64)
+        duration_loss = torch.zeros((), dtype=torch.float64)
+        forward_sum_loss = torch.zeros((), dtype=torch.float64)
+        aux_loss = torch.zeros((), dtype=torch.float64)
 
         batch_size = torch.zeros((), dtype=torch.float64)
 
         num_source_elements = torch.zeros((), dtype=torch.float64)
         num_target_elements = torch.zeros((), dtype=torch.float64)
+        num_target_text_elements = torch.zeros((), dtype=torch.float64)
 
         for batch, batch_loss in zip(batches, losses):
-            loss += float(batch_loss)
+            unit_nll_loss += float(batch_loss["unit_nll_loss"])
+            text_nll_loss += float(batch_loss["text_nll_loss"])
+            duration_loss += float(batch_loss["duration_loss"])
+            forward_sum_loss += float(batch_loss["forward_sum_loss"])
+            aux_loss += float(batch_loss["aux_loss"])
 
             batch_size += batch.batch_size
 
             num_source_elements += batch.num_source_elements()
             num_target_elements += batch.num_target_elements() - batch.batch_size
+            num_target_text_elements += batch.num_target_text_elements() - batch.batch_size
 
-        loss /= num_target_elements
+        unit_nll_loss /= num_target_elements * math.log(2)
+        text_nll_loss /= num_target_text_elements * math.log(2)
+        duration_loss /= num_target_elements
+        forward_sum_loss /= num_target_elements * math.log(2)
+        aux_loss /= num_target_elements * math.log(2)
 
-        self.loss.update(loss, weight=num_target_elements)
-
-        # Mainly exists for compatibility with fairseq's `nll_loss`.
-        self.entropy_loss.update(loss / math.log(2), weight=num_target_elements)
+        self.unit_nll_loss.update(unit_nll_loss, weight=num_target_elements)
+        self.text_nll_loss.update(text_nll_loss, weight=num_target_text_elements)
+        self.duration_loss.update(duration_loss, weight=num_target_elements)
+        self.forward_sum_loss.update(forward_sum_loss, weight=num_target_elements)
+        self.aux_loss.update(aux_loss, weight=num_target_elements)
 
         self.batch_size.update(batch_size * self.gang.size)
 
@@ -103,8 +123,11 @@ class UnitYMetricBag(MetricBag):
 
     def reset_batch_metrics(self) -> None:
         """Reset the batch metrics to their initial state."""
-        self.loss.reset()
-        self.entropy_loss.reset()
+        self.unit_nll_loss.reset()
+        self.text_nll_loss.reset()
+        self.duration_loss.reset()
+        self.forward_sum_loss.reset()
+        self.aux_loss.reset()
         self.batch_size.reset()
         self.elements_per_batch.reset()
         self.elements_per_second.reset()

src/seamless_communication/train/unity2/trainer.py
@@ -16,6 +16,7 @@ from typing import Any, Iterator, Literal, List, Optional, Union, final, Tuple, 
 import submitit
 import torch
 from torch.distributed.fsdp.wrap import transformer_auto_wrap_policy
+from torch.nn.parallel import DistributedDataParallel
 from fairseq2 import setup_extensions
 from fairseq2.checkpoint import FileCheckpointManager
 from fairseq2.gang import Gang, ProcessGroupGang, FakeGang
@@ -59,7 +60,6 @@ from seamless_communication.store import (
     load_unity_text_tokenizer,
     load_unity_unit_tokenizer,
 )
-from seamless_communication.train.utils.distributed import init_process_group
 from seamless_communication.train.unity2.dataset import (
     UnitYDataset,
     load_unity_dataset,
@@ -94,11 +94,12 @@ class UnitYTrainConfig:
     parallelism: Literal["none", "fsdp", "ddp"] = "fsdp"
     """The type of parallelism to use."""
 
-    max_seq_len: int = 4095
+    max_seq_len: int = 4000
     """The maximum source and target sequence lengths."""
 
-    max_num_tokens: int = 4096
-    """The maximum number of tokens per batch."""
+    max_num_tokens: int = 8000
+    """The maximum number of tokens per batch. This is effectively 4k as in fairseq1's batch_size
+    as fairseq1 uses n_frames, here it uses fbank.shape"""
 
     shuffle_window_size: int = 100_000
     """The size of the sliding data shuffle window."""
@@ -157,9 +158,6 @@ class UnitYTrainConfig:
 
     profile: bool = False
     """If ``True``, runs the PyTorch profiler at the beginning of the training."""
-
-    use_submitit: bool = True
-    """If ``True``, Setup the environment ti use Submitit."""
 
     aux_loss_type: Optional[str] = None
     """the auxiliary loss type"""
@@ -238,10 +236,10 @@ class UnitYTrainer(StatefulObjectBag):
             text_tokenizer,
             unit_tokenizer,
             gang,
+            self.dtype,
             config.max_seq_len,
             config.max_num_tokens,
-            # bucket_by_length=True,
-            bucket_by_length=False,
+            bucket_by_length=True,
             shuffle_window_size=config.shuffle_window_size,
             num_prefetch=config.num_prefetch,
             num_accumulate=config.gradient_accumulation,
@@ -277,8 +275,8 @@ class UnitYTrainer(StatefulObjectBag):
             optimizer,
             gang=gang,
             init_scale=128,
-            scale_window=int(2**14 / gang.size / config.gradient_accumulation),
             min_scale=0.00001,
+            gradient_accumulation=config.gradient_accumulation,
             enabled=self.dtype == torch.float16,
         )
 
@@ -376,16 +374,20 @@ class UnitYTrainer(StatefulObjectBag):
 
             losses = []
 
-            with torch.autocast(device_type="cuda", dtype=self.dtype):
-                # Accumulate gradients.
-                for batch_nr, batch in enumerate(batches):
-                    with record_function(f"step_{step_nr}_{batch_nr}_forward"):
-                        loss = self._compute_loss(batch)
+            # Accumulate gradients.
+            for batch_nr, batch in enumerate(batches):
+                with record_function(f"step_{step_nr}_{batch_nr}_forward"):
+                    loss, extras = self._compute_loss(batch)
 
-                    with record_function(f"step_{step_nr}_{batch_nr}_backward"):
-                        self.loss_scaler.backward(loss)
+                if loss.isnan():
+                    logger.error("Detected nan")
+                    logger.info(batch.source_seqs.shape, batch.target_seqs, batch.target_text_seqs, batch.example["split"], batch.example["line_nr"])
+                    continue
 
-                    losses.append(loss.detach())
+                with record_function(f"step_{step_nr}_{batch_nr}_backward"):
+                    self.loss_scaler.backward(loss)
+
+                losses.append(extras)
 
             # Update parameters.
             with record_function(f"step_{step_nr}_optimizer"):
@@ -445,6 +447,7 @@ class UnitYTrainer(StatefulObjectBag):
             self.text_tokenizer,
             self.unit_tokenizer,
             self.gang,
+            self.dtype,
             self.config.max_seq_len,
             self.config.max_num_tokens,
             num_prefetch=self.config.num_prefetch,
@@ -464,7 +467,7 @@ class UnitYTrainer(StatefulObjectBag):
 
             logger.debug("Running validation step %d.", step_nr)
 
-            losses = [self._compute_loss(batch) for batch in batches]
+            losses = [self._compute_loss(batch)[1] for batch in batches]
 
             elapsed_time = step_stopwatch.get_elapsed_time()
 
@@ -549,7 +552,7 @@ def load_unity_trainer(config: UnitYTrainConfig) -> UnitYTrainer:
     stopwatch = Stopwatch(start=True)
 
     setup_logging(
-        log_file=config.output_dir.joinpath("logs/train_{rank}.log"), debug=True
+        log_file=config.output_dir.joinpath("logs/train_{rank}.log"), debug=False
     )
 
     setup_extensions()
@@ -562,7 +565,7 @@ def load_unity_trainer(config: UnitYTrainConfig) -> UnitYTrainer:
     if config.parallelism == "none":
         gang = FakeGang()
     else:
-        gang = init_process_group(config, logger)
+        gang = ProcessGroupGang.init_default_process_group(ok_initialized=True)
 
     checkpoint_manager = FileCheckpointManager(
         config.output_dir.joinpath("checkpoints"),
@@ -628,8 +631,8 @@ def load_unity_trainer(config: UnitYTrainConfig) -> UnitYTrainer:
         to_device(raw_model, gang.device)
 
         model = raw_model
-    elif self.config.parallelism == "fsdp":
-        wrap_policy, ignored_param_names = unity_wrap_policy(model_config, gang)
+    elif config.parallelism == "fsdp":
+        wrap_policy, ignored_param_names = unity_wrap_policy()
 
         model = to_fsdp(
             raw_model,
@@ -643,7 +646,12 @@ def load_unity_trainer(config: UnitYTrainConfig) -> UnitYTrainer:
             log_module(model, logger)
 
     elif config.parallelism == "ddp":
-        model = raw_model  # TODO: to_ddp()
+        model = DistributedDataParallel(
+            raw_model,
+            device_ids=[gang.device],
+        )
+        if gang.rank == 0:
+            log_module(model, logger)
 
     trainer = UnitYTrainer(
         config,