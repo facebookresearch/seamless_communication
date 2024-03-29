Fix float precision in Finetune CLI

src/seamless_communication/cli/m4t/finetune/finetune.py
@@ -126,7 +126,7 @@ def main() -> None:
     args = init_parser().parse_args()
     dist_utils.init_distributed([logger, trainer.logger])
     device = torch.device("cuda")
-    float_dtype = torch.float16
+    float_dtype = torch.float32
     text_tokenizer: NllbTokenizer = load_unity_text_tokenizer(args.model_name)
     unit_tokenizer: UnitTokenizer = load_unity_unit_tokenizer(args.model_name)
     finetune_params = trainer.FinetuneParams(

src/seamless_communication/cli/m4t/finetune/trainer.py
@@ -348,20 +348,21 @@ class UnitYFinetune:
 
     def _train_step(self, batch: dataloader.MultimodalSeqsBatch) -> None:
         """Run one train step"""
-        self.model.train()
-        self.optimizer.zero_grad()
-        tokens, units = self.model(batch)
-        loss = self.calc_loss(batch, tokens, units)
-        if loss.isnan().any().item():
-            logger.error(batch.speech_to_text)
-            raise RuntimeError("Loss is Nan. Terminating.")
-        self.grad_scaler.scale(loss).backward()
-        self.grad_scaler.step(self.optimizer)
-        self.grad_scaler.update()
-        self.lr_scheduler.step()
-        assert batch.speech_to_text.src_tokens is not None
-        self.train_loss_hist.update(1, loss.item())
-        self._train_step_log()
+        with torch.autocast(device_type=self.params.device):
+            self.model.train()
+            self.optimizer.zero_grad()
+            tokens, units = self.model(batch)
+            loss = self.calc_loss(batch, tokens, units)
+            if loss.isnan().any().item():
+                logger.error(batch.speech_to_text)
+                raise RuntimeError("Loss is Nan. Terminating.")
+            self.grad_scaler.scale(loss).backward()
+            self.grad_scaler.step(self.optimizer)
+            self.grad_scaler.update()
+            self.lr_scheduler.step()
+            assert batch.speech_to_text.src_tokens is not None
+            self.train_loss_hist.update(1, loss.item())
+            self._train_step_log()
 
     def _save_model(self) -> None:
         logger.info("Saving model")