decb48ded6346a2f6f29573feffd2f1bac2aca25
Alisha <alisha831maddy@gmail.com>
2024-04-05 09:52:06 -0700

    pipeline segmenter with transcriber

--- src/seamless_communication/segment/silero_vad.py
+++ src/seamless_communication/segment/silero_vad.py
@@ -6,18 +6,9 @@
 
 from __future__ import annotations
 
-import logging
-from pathlib import Path
-from argparse import ArgumentParser, Namespace
-from os import SEEK_END
-from typing import Any, List, Optional, Union
+from argparse import Namespace
 import torch
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s -- %(name)s: %(message)s",
-)
-logger = logging.getLogger(__name__)
 
 class SileroVADSegmenter:  # type: ignore
     def __init__(self, args: Namespace) -> None:
@@ -35,9 +26,9 @@ class SileroVADSegmenter:  # type: ignore
             self.VADIterator,
             self.collect_chunks,
         ) = utils
-        self.sample_rate = getattr(args, 'sample_rate', 16000)
-        self.chunk_size_sec = getattr(args, 'chunk_size_sec', 10)
-        self.pause_length = getattr(args, 'pause_length', 0.5)
+        self.sample_rate = getattr(args, "sample_rate", 16000)
+        self.chunk_size_sec = getattr(args, "chunk_size_sec", 10)
+        self.pause_length = getattr(args, "pause_length", 0.5)
 
     def segment_long_input(self, audio) -> None:
         """
@@ -46,18 +37,21 @@ class SileroVADSegmenter:  # type: ignore
         max_segment_length_samples = self.chunk_size_sec * self.sample_rate
         pause_length_samples = self.pause_length * self.sample_rate
 
-        speech_timestamps = self.get_speech_timestamps(audio, self.model, sampling_rate=self.sample_rate)
+        speech_timestamps = self.get_speech_timestamps(
+            audio, self.model, sampling_rate=self.sample_rate
+        )
 
         segments = []
         current_segment = []
 
         for segment in speech_timestamps:
-            start_samples = segment['start']
-            end_samples = segment['end']
+            start_samples = segment["start"]
+            end_samples = segment["end"]
 
-            if (current_segment and
-                (end_samples - current_segment[0] > max_segment_length_samples or
-                start_samples - current_segment[1] > pause_length_samples)):
+            if current_segment and (
+                end_samples - current_segment[0] > max_segment_length_samples
+                or start_samples - current_segment[1] > pause_length_samples
+            ):
                 segments.append(current_segment)
                 current_segment = []
 
@@ -67,6 +61,5 @@ class SileroVADSegmenter:  # type: ignore
                 current_segment[1] = end_samples
         if current_segment:
             segments.append(current_segment)
-     
+
         return segments
-      
