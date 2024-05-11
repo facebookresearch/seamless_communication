3839e24bd37e9999a3b01527c0c06f46b33d1b3c
Alisamar Husain <zrthxn@gmail.com>
2024-05-11 13:22:11 +0200

    Model Fixes (#3)
    * Small fixes
    
    * Argyment
    
    * Batch size
    
    * Model shape
    
    * unsqueeze
    
    * Long
    
    * Float tensor
    
    * Float tensor
    
    * DEvice
    
    * While set
    
    * src_lengths
    
    * Refactor

--- src/seamless_communication/cli/m4t/classification_head/model.py
+++ src/seamless_communication/cli/m4t/classification_head/model.py
@@ -1,19 +1,19 @@
 from torch import nn
 
 class ClassificationHead(nn.Module):
-    def __init__(self, input_dim, n_layers, n_classes):
+    def __init__(self, embed_dim, n_layers, n_classes):
         super(ClassificationHead, self).__init__()
         self.num_languages = n_classes
         self.num_layers = n_layers
-        self.input_dim = input_dim
-        self.hidden_dim = self.input_dim
         
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads=1)
         self.layers = nn.ModuleList(
-            [ nn.Linear(input_dim, input_dim) for _ in range(n_layers) ] + \
-            [ nn.Linear(input_dim, n_classes) ])
+            [ nn.Linear(embed_dim, embed_dim) for _ in range(n_layers) ] + \
+            [ nn.Linear(embed_dim, n_classes) ])
 
     def forward(self, x):
+        x, _ = self.attn(x, x, x)
         for layer in self.layers:
             x = nn.functional.relu(layer(x))
-        return nn.functional.softmax(x)
+        return nn.functional.softmax(x[:, 0]).float()
     
