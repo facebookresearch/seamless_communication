d05d40881cec36f6a6d9a53b6aac5073c9f60666
Alisha <alisha831maddy@gmail.com>
2024-05-13 14:17:17 -0700

    fix unit test

--- tests/unit/segment/test_silero_vad.py
+++ tests/unit/segment/test_silero_vad.py
@@ -10,23 +10,36 @@ from unittest.mock import Mock
 from seamless_communication.segment.silero_vad import SileroVADSegmenter, Segment
 import numpy as np
 
+
 class TestSileroVADSegmenter(unittest.TestCase):
     def test_init_works(self):
-        args = Namespace(sample_rate=16000, chunk_size_sec=10, pause_length=0.5)
-        segmenter = SileroVADSegmenter(args)
+        segmenter = SileroVADSegmenter(
+          sample_rate=16000, 
+          chunk_size_sec=10, 
+          pause_length=0.5)
         self.assertEqual(segmenter.sample_rate, 16000)
         self.assertEqual(segmenter.chunk_size_sec, 10)
         self.assertEqual(segmenter.pause_length, 0.5)
 
+
     def test_segment_long_input(self):
-        args = Namespace(sample_rate=16000, chunk_size_sec=10, pause_length=0.5)
-        self.segmenter = SileroVADSegmenter(args)
-        self.segmenter.get_speech_timestamps = Mock(return_value=[{"start": 0, "end": 10000}, {"start": 20000, "end": 30000}])
+        self.segmenter = SileroVADSegmenter(
+          sample_rate=16000, 
+          chunk_size_sec=10, 
+          pause_length=0.5)
+        self.segmenter.get_speech_timestamps = Mock(
+          return_value=[{0: 0, 1: 10000}, 
+          {0: 20000, 1: 30000}])
         segments = self.segmenter.segment_long_input(audio=None)
         expected_segments = [[0, 10000], [20000, 30000]]
         self.assertEqual(segments, expected_segments)
 
-    def test_recursive_split():
+
+    def test_recursive_split(self):
+        segmenter = SileroVADSegmenter(
+          sample_rate=16000, 
+          chunk_size_sec=10,
+          pause_length=0.5)
         sgm = Segment(0, 10000, np.random.rand(10000))
         segments = []
         max_segment_length = 5000
@@ -34,7 +47,13 @@ class TestSileroVADSegmenter(unittest.TestCase):
         window_size_samples = 100
         threshold = .5
 
-        SileroVADSegmenter.recursive_split(sgm, segments, max_segment_length, min_segment_length, window_size_samples, threshold)
+        segmenter.recursive_split(
+          sgm, 
+          segments, 
+          max_segment_length, 
+          min_segment_length, 
+          window_size_samples, 
+          threshold)
 
         assert all([seg.duration < max_segment_length for seg in segments])
         assert all([seg.duration > min_segment_length for seg in segments])
