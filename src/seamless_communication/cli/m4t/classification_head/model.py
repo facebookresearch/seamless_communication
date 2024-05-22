d21645c90bda9bbef1d0e7486c1df93908348a53
Ruslan Mavlyutov <mavlyutov@meta.com>
2024-05-22 04:36:25 +0000

    fixes

--- src/seamless_communication/cli/m4t/classification_head/model.py
+++ src/seamless_communication/cli/m4t/classification_head/model.py
@@ -7,15 +7,11 @@ class ClassificationHead(nn.Module):
         super(ClassificationHead, self).__init__()
         self.num_languages = n_classes
         self.num_layers = n_layers
-
-        self.attn = nn.MultiheadAttention(embed_dim, num_heads=n_heads)
         self.layers = nn.ModuleList(
             [
                 nn.Sequential(
                     nn.Linear(embed_dim, embed_dim),
-                    nn.BatchNorm1d(embed_dim),  # normalize batch
-                    nn.ReLU(),  # activation function
-                    nn.Dropout(0.1),  # prevent overfitting
+                    nn.ReLU(),
                 )
                 for _ in range(n_layers)
             ]
@@ -24,7 +20,6 @@ class ClassificationHead(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # (Batch, Seq, Embed)
-        x, _ = self.attn(x, x, x)
         x = x[:, 0]
         for layer in self.layers:
             x = layer(x)
